73255a65f36074fea09d365c1220efa9d1488645
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-05 16:19:17 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1240 ms, Memory: 59236 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        // (x,y) 나열 및 분류
+        // Comparator의 비교 방식은 양수 음수 파악으로 진행
+        // x끼리 같으면 y끼리 비교하도록
+        // 다르면 x끼리 비교하도록
+        Arrays.sort(arr, (arr1, arr2) -> {
+            if(arr1[0] == arr2[0]) {
+                return arr1[1] - arr2[1];
+            }else{
+                return arr1[0] - arr2[0];
+            }
+        });
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
