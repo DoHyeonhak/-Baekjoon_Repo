14e445ca2fa36bb86ab61b83096e5c935080e987
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-15 17:41:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,27 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // N 킬로그램 배달 (3, 5단위)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int cnt = 0;
-
-        while(n > 0){
-            if(n%5 == 0){   // 5의 배수
-                cnt += n/5;
+        // 3, 5
+        // 배달 최소 봉지
+        while(N>0){
+            // 5로 나누었을 때 나머지가 0이 아니면 3으로 계속 빼기
+            if(N%5==0){
+                cnt += N/5;
                 break;
-            }else{  // 5의 배수가 아닌 경우
-                n -= 3;
-                cnt ++;
+            }else{
+                N-=3;
+                cnt++;
             }
-            if(n < 0){
+            if(N<0){
                 cnt = -1;
                 break;
             }
         }
         System.out.println(cnt);
     }
-
 }
+
