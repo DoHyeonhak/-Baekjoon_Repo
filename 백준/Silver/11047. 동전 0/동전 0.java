81dde6353df2d98c1f81d513b6d16b4f966210a7
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-16 18:03:08 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,20 +4,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         int cnt = 0;
-        for(int i = n-1; i >= 0; i--) {
-            if(k >= arr[i]){
-                cnt += k/arr[i];
-                k %= arr[i];
-            }
+        for(int i = N-1; i >= 0; i--) {
+            cnt += M/arr[i];
+            M %= arr[i];
         }
         System.out.println(cnt);
+
     }
 }
+
