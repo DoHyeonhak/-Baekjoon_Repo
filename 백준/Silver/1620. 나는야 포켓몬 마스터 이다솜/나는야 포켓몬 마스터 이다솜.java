7cc44fa2f145e20f5a687509eee16cb027494978
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-17 16:54:31 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1000 ms, Memory: 46640 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -5,28 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        HashMap<String, Integer> names = new HashMap<>();
+        int N = Integer.parseInt(st.nextToken());   // 듣
+        int M = Integer.parseInt(st.nextToken());   // 보
+        HashMap<String, Integer> map = new HashMap<>();
         String[] arr = new String[N+1];
-        String line;
-
-        for (int i = 0; i < N; i++) {
-            line = br.readLine();
-            arr[i] = line;
-            names.put(arr[i], i+1);
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            arr[i] = s;
+            map.put(s, i+1);
         }
-
         for(int i = 0; i < M; i++) {
-            line = br.readLine();
-            // ASCII CODE 적용
-            if(line.charAt(0) > 47 && line.charAt(0) < 58) {    // 숫자인 경우
-                System.out.println(arr[Integer.parseInt(line)-1]);
-            }else{  // 문자인 경우
-                System.out.println(names.get(line));
+            String s = br.readLine();
+            if(map.containsKey(s)) {
+                System.out.println(map.get(s));
+            }else{
+                System.out.println(arr[Integer.parseInt(s)-1]);
             }
         }
     }
 }
+
