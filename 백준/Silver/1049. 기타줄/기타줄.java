12a1e4937a732f069048ab07cce993f06dae7486
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-17 17:13:44 +0900

    [Silver IV] Title: 기타줄, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -1,25 +1,27 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 기타줄
+        int M = Integer.parseInt(st.nextToken());   // 브랜드
+        // 6개 패키지 or 낱개
         int pack = Integer.MAX_VALUE;
-        int one = Integer.MAX_VALUE;
-        for(int i = 1; i <= M; i++) {
-            pack = Math.min(pack, sc.nextInt());
-            one = Math.min(one, sc.nextInt());
+        int each = Integer.MAX_VALUE;
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            pack = Math.min(pack, Integer.parseInt(st.nextToken()));
+            each = Math.min(each, Integer.parseInt(st.nextToken()));
         }
         int ans = 0;
         while(N>=6){
-            ans += Math.min(one*6, pack);
+            ans += Math.min(pack, each*6);
             N -= 6;
         }
-        if(N>0){
-            ans += Math.min(one*N, pack);
-        }
+        ans += Math.min(pack, each*N);
         System.out.println(ans);
     }
-
 }
+
