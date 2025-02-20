495e70f700bdcf072f16b4899ec7d051d443547a
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-20 17:29:04 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,28 +1,38 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        for(int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        Stack<Character> stack;
+        for(int i = 0; i < N; i++) {
+            stack = new Stack<>();
             String s = br.readLine();
-            int cnt = 0;
+            String ans = "";
+            boolean valid = true;   // break된 경우 확인용(false인 경우 NO를 위함)
             for(int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == '('){
-                    cnt += 1;
-                }else{
-                    cnt -= 1;
+                if(s.charAt(j) == '(') {
+                    stack.push(s.charAt(j));
+                }else if(stack.isEmpty()){  // ')'이면 pop해야 되는데 empty이면 break
+                    ans = "NO";
+                    valid = false;
+                    break;
+                }else{  // ')'
+                    stack.pop();
                 }
-                if(cnt < 0) break;
             }
-            if(cnt == 0){
-                System.out.println("YES");
+
+            if(stack.isEmpty()&&valid){    // 괄호가 정상적으로 닫힌 경우
+                ans = "YES";
             }else{
-                System.out.println("NO");
+                ans = "NO";
             }
+            System.out.println(ans);
         }
 
-    }
 
+
+    }
 }
