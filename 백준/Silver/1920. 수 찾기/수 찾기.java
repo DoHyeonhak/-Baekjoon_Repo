8ea3920d65876388eea392e5be8c9b216496e84a
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-23 20:29:41 +0900

    [Silver IV] Title: 수 찾기, Time: 1004 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,26 +1,47 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // 이중 반복문만 사용한다고 하면 시간 초과 문제가 계속 생김
-        // 시간복잡도가 맞은 이진탐색법 사용
-        Scanner sc = new Scanner(System.in);
-        int i, j;
-        int N = sc.nextInt();
-        int[] arr1 = new int[N];
-        for(i = 0; i < N; i++) {
-            arr1[i] = sc.nextInt();
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        // Binary Search 
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr1);
-        int M = sc.nextInt();
-        int[] arr2 = new int[M];
-        for(i = 0; i < M; i++) {
-            arr2[i] = sc.nextInt();
-            if(Arrays.binarySearch(arr1, arr2[i]) >= 0) { // binary search
+        
+        Arrays.sort(arr);
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if(binarySearch(arr, target) >= 0){
                 System.out.println(1);
             }else{
                 System.out.println(0);
             }
         }
     }
+    static int binarySearch(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(key < arr[mid]) {  // key가 더 작은 경우
+                right = mid - 1;
+            }else if(key > arr[mid]) {    // key가 더 큰 경우
+                left = mid + 1;
+            }else{  // 같은 경우
+                return mid;
+            }
+        }
+        return -1;
+    }
+
 }
