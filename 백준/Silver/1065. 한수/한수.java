80fb87dc79ffb036f3d768c1b25d01a6be8d3029
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-02-28 17:04:36 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -4,25 +4,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 한수
-        // 1~99는 다 한수
-        // 100부터는 확인해봐야 한다
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
         int ans = 99;
-        if(N<100){
-            System.out.println(N);
-        }else{  // 100 이상인 경우
+        if(N>=100){
             for(int i = 100; i <= N; i++){
                 int a = i/100;
-                int b = (i/10)%10;
+                int b = (i%100)/10;
                 int c = i%10;
-                if((a-b)==(b-c)){   // 등차수열이라면
+                if((b-a)==(c-b)) {
                     ans++;
                 }
             }
-            System.out.println(ans);
+        }else{
+            ans = N;
         }
+        System.out.println(ans);
     }
-
 }
