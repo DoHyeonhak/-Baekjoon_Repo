c308205862126d07354217f9137c80b82c5200f4
DoHyeonhak <162457162+DoHyeonhak@users.noreply.github.com>
2025-03-04 22:08:23 +0900

    [Bronze I] Title: 2진수 8진수, Time: 356 ms, Memory: 38864 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,21 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder ans = new StringBuilder();
-        String N = sc.nextLine();
-        int size = N.length();
-        if(size%3 == 1) N = "00" + N;
-        if(size%3 == 2) N = "0" + N;
-
-        for(int i = 0; i < N.length(); i+=3){
-            String temp = N.substring(i, i+3);
-            int a = (N.charAt(i)-'0')*4;
-            int b = (N.charAt(i+1)-'0')*2;
-            int c = (N.charAt(i+2)-'0');
-            ans.append(a+b+c);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if(str.length()%3==1) str = "00"+str;
+        if(str.length()%3==2) str = "0"+str;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i+=3){
+            int num = Integer.parseInt(str.substring(i, i+3));
+            int a = (num/100)*4;
+            int b = ((num/10)%10)*2;
+            int c = (num%10);
+            sb.append(a+b+c);
         }
-        System.out.println(ans);
+        System.out.println(sb.toString());
     }
 }
